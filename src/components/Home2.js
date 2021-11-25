03b2651df4af7b93aa90817c89efd1277538c616
linxxnint <vasteple@gmail.com>
2021-11-25 19:37:26 +0800

    add some images

--- src/components/Home2.js
+++ src/components/Home2.js
@@ -78,12 +78,12 @@ const Home2 = (props) => {
 
               {/* ///// slide  //// Fun woth slides?????  */}
               <div ref={sliderRef} className="keen-slider">
-                <div className="keen-slider__slide number-slide1">1</div>
-                <div className="keen-slider__slide number-slide2">2</div>
-                <div className="keen-slider__slide number-slide3">3</div>
-                <div className="keen-slider__slide number-slide4">4</div>
-                <div className="keen-slider__slide number-slide5">5</div>
-                <div className="keen-slider__slide number-slide6">6</div>
+                <div className="keen-slider__slide number-slide1"></div>
+                <div className="keen-slider__slide number-slide2"></div>
+                <div className="keen-slider__slide number-slide3"></div>
+                <div className="keen-slider__slide number-slide4"></div>
+                <div className="keen-slider__slide number-slide5"></div>
+                <div className="keen-slider__slide number-slide6"></div>
               </div>
               {/* ///// slide  //// */}
               <Stack
